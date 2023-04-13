e357829c9d06af9ee6f6a794342a813b9a203a07
Leopold Spanovic <smuk3c@Leopolds-MacBook-Pro.fritz.box>
2023-04-13 11:50:27 +0200

    Small changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
     "./src/components/**/*.{js,jsx,ts,tsx}",
     "./src/svgs/**/*.{js,jsx,ts,tsx}",
   ],
+  purge: [
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.ts",
+    "./src/**/*.tsx",
+  ],
   theme: {
     extend: {
       fontFamily: {
