954dbb1945629da6dbc7f30a768f20a46ca38d69
Leopold Spanovic <smuk3c@Leopolds-MacBook-Pro.fritz.box>
2023-04-13 12:17:29 +0200

    Adding tailwind at top

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/svgs/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
