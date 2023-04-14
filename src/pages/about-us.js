5f143e2e07d6b3c24f12a3f13304353d003fc2c9
Leopold Spanovic <smuk3c@Leopolds-MacBook-Pro.fritz.box>
2023-04-14 14:48:46 +0200

    Adjusting front page

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -64,7 +64,7 @@ const AboutUsPage = ({ data }) => (
         </div>
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
           <GatsbyImage
-            className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+            className="w-[48rem] max-w-none rounded-xl bg-neutral-50 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
             placeholder="blurred"
             width="fullWidth"
             image={getImage(data.hero)}
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,11 +28,37 @@ const IndexPage = ({ data }) => (
       learnMore={ROUTES.MODEL_3}
       title="for Model 3"
     />
-    <div className="bg-white block w-screen md:snap-center flex flex-col">
+    <div className="bg-white relative isolate  block w-screen md:snap-center flex flex-col">
+      <div className="absolute inset-0 -z-10 overflow-hidden">
+        <svg
+          className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="e813992c-7d03-4cc4-a2bd-151760b470a0"
+              width={200}
+              height={200}
+              x="50%"
+              y={-1}
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M100 200V.5M.5 .5H200" fill="none" />
+            </pattern>
+          </defs>
+          <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+            <path
+              d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+              strokeWidth={0}
+            />
+          </svg>
+          <rect width="100%" height="100%" strokeWidth={0} fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
+        </svg>
+      </div>
       <div className="mx-auto w-4/5 px-6 lg:flex lg:items-center lg:justify-between lg:px-8 grow">
         <div className="flex flex-col justify-center gap-8 h-screen snap-center md:snap-none">
           <GatsbyImage
-            className="h-screen w-screen rounded bg-neutral-50 p-10"
+            className="h-screen w-screen rounded bg-neutral-50 p-10 shadow-xl ring-1 ring-gray-400/10"
             objectPosition="center top"
             placeholder="blurred"
             image={getImage(data.modelYProduct)}
@@ -48,7 +74,7 @@ const IndexPage = ({ data }) => (
         </div>
         <div className="flex flex-col justify-center gap-8 h-screen snap-center md:snap-none">
           <GatsbyImage
-            className="h-screen w-screen rounded bg-neutral-50 p-10"
+            className="h-screen w-screen rounded bg-neutral-50 p-10 shadow-xl ring-1 ring-gray-400/10"
             objectPosition="center top"
             placeholder="blurred"
             image={getImage(data.model3Product)}
